41f3a3d1173d2488a0180d769431f22c8380cb73
sadias-dreamweb <sadiatuzjohora11@gmail.com>
2022-04-17 22:59:54 -0400

    not found page edited

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,9 +4,9 @@ import pic from '../../images/emoji.jpg'
 
 const NotFound = () => {
     return (
-        <div className='container'>
+        <div className='container  mb-60'>
             <img className='mx-auto mt-20' style={{ height: '10rem' }} src={pic} alt="" />
-            <div className='text-center' >
+            <div className='text-center pb-72' >
                 <h2>Sorry! The page you are looking for is not found!</h2>
                 <Link to='/'><button className='btn btn-warning text-white'>Go Back to Home</button></Link>
             </div>
